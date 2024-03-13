1e47d437a0e6dc55ed151ad6c3c4ab7e7a6efda2
joelnyongesa <joelnyongesa148@gmail.com>
2024-03-13 18:14:42 +0300

    Initial commit: getting started with Go

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Declaring a variable: Strings
+	var name = "Joel Nyongesa"
+	var school = ""
+	company := "An10"
+
+	fmt.Println(name, school, company)
+
+	// Numbers: Ints and Floats in Go
+	var age int = 20
+	var siblings int = 5
+	salary := 9000
+
+	fmt.Println(age, siblings, salary)
+
+	// Bits and Memory in Go
+	var schools int
+}
